1578639a8a3d1f88fe6b931f8bed0ff3d2c5c449
zhuangzz <1206838830@qq.com>
2017-09-28 15:49:42 +0800

    2

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React ,{ Component } from 'react'
+import './app.css'
+import CommentBox from '../CommentBox/CommentBox'
+import PostBody from '../PostBody/PostBody'
+
+class App extends Component{
+  render(){
+    return(
+      <div className="app">
+        app
+      </div>
+    )
+  }
+}
+export default App
---
+++ src/components/PostBody/PostBody.js
@@ -0,0 +1,14 @@
+import React ,{ Component } from 'react'
+import './post-body.css'
+
+
+class PostBody extends Component{
+  render(){
+    return(
+      <div className="post-body">
+        postbody
+      </div>
+    )
+  }
+}
+export default PostBody
